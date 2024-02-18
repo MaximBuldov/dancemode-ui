2db1991d5d6666d5e5e188f6a8b539868d5d1fd3
Maxim <Buldov@mail.ru>
2024-02-17 22:18:34 -0800

    order note

--- src/components/payments-products.tsx
+++ src/components/payments-products.tsx
@@ -3,6 +3,7 @@ import { IKeys, IROrder, IROrderProduct } from 'models';
 import type { ColumnsType } from 'antd/es/table';
 import dayjs from 'dayjs';
 import { useUpdateOrder } from 'hooks';
+import { useState } from 'react';
 
 import { Price } from './ui';
 
