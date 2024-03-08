feba3ab657e1e9e3b752e1082a4ad2a05f07ce14
Maxim <Buldov@mail.ru>
2024-03-07 23:00:43 -0800

    fix home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { userStore } from 'stores';
 import { observer } from 'mobx-react-lite';
+import { Calendar } from 'antd';
 
 import { Login } from './login';
 import { Classes } from './classes';
 
 export const Home = observer(() => {
-  const { isAuth } = userStore;
-  return isAuth ? <Classes /> : <Login />;
+  const { isAuth, isAdmin } = userStore;
+  return isAuth ?
+    isAdmin ? <Calendar /> : <Classes />
+    : <Login />;
 });
