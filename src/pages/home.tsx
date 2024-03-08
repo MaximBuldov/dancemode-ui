0f2eb6ec456e2b20f131f8ad3a7e3c48b5d23e88
Maxim <Buldov@mail.ru>
2024-03-07 23:04:18 -0800

    Fix home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { userStore } from 'stores';
 import { observer } from 'mobx-react-lite';
-import { Calendar } from 'antd';
 
 import { Login } from './login';
 import { Classes } from './classes';
+import { Calendar } from './calendar';
 
 export const Home = observer(() => {
   const { isAuth, isAdmin } = userStore;
